4d61c49e3057bf84a6d2a9556b9f0cd30c847e22
Nadav <nadavvivsa@gmail.com.com>
2022-11-23 02:05:56 +0200

    a

--- src/cmps/course-info.jsx
+++ src/cmps/course-info.jsx
@@ -3,8 +3,7 @@ export const CourseInfo=()=>{
         <section className="course-info">
             <h1>החוגים והסדנאות שלי</h1>
             <p>יחדיו נלמד ונתרגל תהליכים המסייעים להתפתחות מדויקת ואיכותית של תינוקך , נרקוד, נצחק ונקנה הרגלי שינה בריאים. כל זאת תוך המתקדות בהתפתחות מוטורית מדויקת של תינוקך בהתאמה לשלב ההתפתחותי בו הוא נמצא.</p>
-            <iframe className="video-1" src='https://res.cloudinary.com/ds8xkm0ue/video/upload/v1669138407/Shosh%20Rize/%D7%9E%D7%A7%D7%95%D7%95%D7%94_%D7%A9%D7%90%D7%97%D7%A8%D7%95%D7%9F_vaeoki.mp4'></iframe>
-            <iframe src="" frameborder="0"></iframe>
+            <video controls={true} autoPlay={true} className="video-1" src='https://res.cloudinary.com/ds8xkm0ue/video/upload/v1669138407/Shosh%20Rize/%D7%9E%D7%A7%D7%95%D7%95%D7%94_%D7%A9%D7%90%D7%97%D7%A8%D7%95%D7%9F_vaeoki.mp4'></video>
         </section>
     )
 }
